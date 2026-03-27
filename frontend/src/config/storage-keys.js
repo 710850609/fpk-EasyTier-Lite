485471159f11c24c06ff454866486c15a2f67aa0
BoB <710850609@qq.com>
2026-03-27 16:10:03 +0800

    fix

--- frontend/src/config/storage-keys.js
+++ frontend/src/config/storage-keys.js
@@ -4,12 +4,12 @@
  */
 
 // VConsole 相关
-export const VCONSOLE_ENABLED_KEY = 'easytier-vconsole-enabled'
+export const VCONSOLE_ENABLED_KEY = 'etLite-vconsole-enabled'
 
 // 主题相关
-export const THEME_MODE_KEY = 'easytier-theme'
+export const THEME_MODE_KEY = 'etLite-theme'
 
 // Nodes 页面设置
-export const NODES_SELECTED_COLUMNS_KEY = 'nodes-selected-columns'
-export const NODES_SELECTED_NODE_TYPES_KEY = 'nodes-selected-node-types'
-export const NODES_REFRESH_STEP_KEY = 'nodes-refresh-step'
+export const NODES_SELECTED_COLUMNS_KEY = 'etLite-nodes-columns'
+export const NODES_SELECTED_NODE_TYPES_KEY = 'etLite-nodes-rows'
+export const NODES_REFRESH_STEP_KEY = 'etLite-nodes-refresh-step'
